7142db93c12dc3d6ceb7aa80b25dd113c33f9152
Muhab Gamal <99745309+muhabme@users.noreply.github.com>
2023-10-08 10:51:42 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+char so;
+for (so = 'a' ; so <= 'z' ; so++)
+putchar(so);
+putchar('\n');
+return (0);
+}
