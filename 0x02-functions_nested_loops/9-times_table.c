d028b965f1b758982ac57ced41ad93bc95c40848
Muhab Gamal <99745309+muhabme@users.noreply.github.com>
2023-10-10 14:45:41 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,22 +6,22 @@
  */
 void times_table(void)
 {
-    int row, column, result;
+	int row, column, result;
 
-    for (row = 0; row < 10; row++)
-    {
-        for (column = 0; column < 10; column++)
-        {
-            result = row * column;
-            if (column == 0)
-            {
-                printf("%d", result);
-            }
-            else
-            {
-                printf(", %2d", result);
-            }
-        }
-        printf("\n");
-    }
+	for (row = 0; row < 10; row++)
+	{
+		for (column = 0; column < 10; column++)
+		{
+			result = row * column;
+			if (column == 0)
+			{
+				printf("%d", result);
+			}
+			else
+			{
+				printf(", %2d", result);
+			}
+		}
+		printf("\n");
+	}
 }
