c75951e27e16d4e666ff5b1bba7269e31a535c03
Muhab Gamal <99745309+muhabme@users.noreply.github.com>
2023-10-10 14:40:11 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _abs - function give the absolute
+*
+* @n: takes in integer type input for function
+*
+* Return: always postive
+*/
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
