07c888548f5fc80cc9dc057810e915456b53d94f
Muhab Gamal <99745309+muhabme@users.noreply.github.com>
2023-10-10 14:34:43 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print a - z
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+int ch;
+for (ch = 'a' ; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
