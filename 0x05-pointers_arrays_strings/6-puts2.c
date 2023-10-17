2cce8a442cbaaf452f28353ef5362fb38e9d114e
Muhab Gamal <99745309+muhabme@users.noreply.github.com>
2023-10-17 10:12:53 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* puts2 - Prints every other character of a string
+* @str: The string to be treated
+*
+* Return: void
+*/
+void puts2(char *str)
+{
+int length = 0;
+int i;
+
+while (str[length] != '\0')
+{
+length++;
+}
+
+for (i = 0; i < length; i += 2)
+{
+_putchar(str[i]);
+}
+
+_putchar('\n');
+}
