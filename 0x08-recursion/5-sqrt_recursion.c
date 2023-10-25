c90ac4d7cf9a34f00ab76e1a7b7126b392a2909d
MuhabGamal <muhabme@gmail.com>
2023-10-26 00:42:55 +0300

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,11 +9,11 @@ int helper(int n, int guess);
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
-    if (n == 0 || n == 1)
-        return (n);
-    return (helper(n, 1));
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (helper(n, 1));
 }
 
 /**
@@ -24,9 +24,9 @@ int _sqrt_recursion(int n)
  */
 int helper(int n, int guess)
 {
-    if (guess * guess == n)
-        return (guess);
-    if (guess * guess > n)
-        return (-1);
-    return (helper(n, guess + 1));
+	if (guess * guess == n)
+		return (guess);
+	if (guess * guess > n)
+		return (-1);
+	return (helper(n, guess + 1));
 }
