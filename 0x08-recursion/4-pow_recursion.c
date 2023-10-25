bb8951f5193744dfe682498e5fb22e3a540dfbb6
MuhabGamal <muhabme@gmail.com>
2023-10-26 00:27:26 +0300

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,12 +7,15 @@
  *
  * Return: Result of x^y, -1 if y is negative.
  */
- 
 int _pow_recursion(int x, int y)
 {
-if (y < 0)
-return -1;
-if (y == 0)
-return 1;
-return x * _pow_recursion(x, y - 1);
+    if (y < 0)
+    {
+        return -1;
+    }
+    if (y == 0)
+    {
+        return 1;
+    }
+    return x * _pow_recursion(x, y - 1);
 }
