01bb4c3e0ba8567d4c6d7abd3f3f7a38c32bbf53
MuhabGamal <muhabme@gmail.com>
2023-10-26 00:41:08 +0300

    is prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,9 +9,9 @@
 int is_prime_number(int n)
 {
 	if (n <= 1)
-		return 0;
+		return (0);
 
-	return is_prime_helper(n, 2);
+	return (is_prime_helper(n, 2));
 }
 
 /**
@@ -24,10 +24,10 @@ int is_prime_number(int n)
 int is_prime_helper(int n, int divisor)
 {
 	if (divisor >= n)
-		return 1;
+		return (1);
 
 	if (n % divisor == 0)
-		return 0;
+		return (0);
 
-	return is_prime_helper(n, divisor + 1);
+	return (is_prime_helper(n, divisor + 1));
 }
