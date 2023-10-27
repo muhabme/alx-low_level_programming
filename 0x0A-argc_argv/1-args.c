ddf1b22b3f44a04715581ad0d6dfd07a2fca7a1c
MuhabGamal <muhabme@gmail.com>
2023-10-27 22:44:21 +0300

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: number of command line arguments
+ * @argv: command line arguments
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+    (void) argv;  /* Unused parameter */
+    printf("%d\n", argc - 1);
+    return (0);
+}
